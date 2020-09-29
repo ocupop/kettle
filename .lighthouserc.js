ecb95b2e7a89c17011ed5493f6d9b79942bfb6c7
tombeck <t@ocupop.com>
2020-09-29 10:31:37 -0700

    adding lighthouse ci

---
+++ .lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      // collect options here
+    },
+    assert: {
+      // assert options here
+    },
+    upload: {
+      // upload options here
+      target: 'temporary-public-storage',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
+    },
+  },
+};
